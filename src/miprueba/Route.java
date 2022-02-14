a393ca3f15d5950742a73f82a6d1f7f26627633d
carlosgitub <carlos.dh08@gmail.com>
2022-02-14 07:46:20 -0500

    changes comments v.002

--- src/miprueba/Route.java
+++ src/miprueba/Route.java
@@ -2,19 +2,23 @@ package miprueba;
 
 
 public enum Route {
-//
+
+    // main use of overriding enumerations
     A("A", 0),
     B("B", 1),
     C("C", 2),
     D("D", 3),
     E("E", 4),
 
+    // represents any of a city
     ANY("", -1),
     OPTIONAL("", -2);
 
+    // represents a city or no city
     private int index;
     private String name;
 
+    // all case routes
     protected static final Route[][] TRAINS = {
             {A, B, C},
             {A, D},
@@ -27,6 +31,8 @@ public enum Route {
             {B, B},
             {C, C}
     };
+
+    // use to constructors, (graph matrix)
     static final int[][] ROUTE_TASK = {
             {0, 5, 0, 5, 7},
             {0, 0, 4, 0, 0},
@@ -45,6 +51,10 @@ public enum Route {
         return name;
     }
 
+    /* *
+     *	>= 0: normal cities.
+     *	< 0: else status.
+     */
     public int getIndex() {
         return index;
     }
