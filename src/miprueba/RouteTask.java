87277b5aac0f2b026ed58c42764c1c82cab66279
carlosgitub <carlos.dh08@gmail.com>
2022-02-14 07:46:45 -0500

    changes comments v.002

--- src/miprueba/RouteTask.java
+++ src/miprueba/RouteTask.java
@@ -1,5 +1,7 @@
 package miprueba;
 
+import org.jetbrains.annotations.NotNull;
+
 import static miprueba.Route.OPTIONAL;
 
 public class RouteTask {
@@ -9,7 +11,15 @@ public class RouteTask {
         this.circuit = circuit;
     }
 
-    public int routeDistance(Route[] route) throws Exception {
+    /**
+     * Return total distance of the given route. Throw an Exception if no path.
+     *
+     * @param route the given route.
+     * @return total distance of the route.
+     * @return return the route Distance
+     * @throws Exception if no path.
+     */
+    public int routeDistance(Route @NotNull [] route) throws Exception {
         int distance = 0;
 
         int idx1 = route[0].getIndex();
@@ -25,35 +35,62 @@ public class RouteTask {
         return distance;
     }
 
-
-
+    /**
+     * Return the number of trips of the given route.
+     * <p>
+     * Return the number of trips of the given route.
+     *
+     * @param route the given route.
+     * @return the number of trips of the route.
+     */
     public int numberOfTrips(Route[] route) {
         return numberOfTrips(route, 0);
     }
 
-    public int shortDistance(Route origin, Route dest) {
+    /**
+     * Return the distance of the shortest route between the given cities.
+     *
+     * @param origin the origin city (start city).
+     * @param dest   the destination city.
+     * @return the distance of the shortest route between origin and destination.
+     */
+    public int shortDistance(@NotNull Route origin, @NotNull Route dest) {
         boolean[][] flags = new boolean[circuit.length][circuit.length];
         int shortest = shortDistance(origin.getIndex(), dest.getIndex(), flags, 0, 0, origin == dest);
         return shortest;
     }
 
-    public int numberOfRoutes(Route origin, Route dest, int maxDistance) {
+    /**
+     * Return the number of routes between the given cities which distance is less than maxDistance
+     *
+     * @param origin      the origin city.
+     * @param dest        the destination city.
+     * @param maxDistance the max distance of the found routes.
+     * @return the number of routes.
+     */
+    public int numberOfRoutes(@NotNull Route origin, @NotNull Route dest, int maxDistance) {
         int number = numberOfRoutes(origin.getIndex(), dest.getIndex(), maxDistance, 0, origin == dest);
         return number;
     }
 
-    private int numberOfTrips(Route[] route, int offset) {
+
+    private int numberOfTrips(Route @NotNull [] route, int offset) {
+
         // we are at the last city on the route.
         if (offset == route.length - 1) return 1;
+
         // try to go forward to next city.
         Route start = route[offset];
         Route next = route[offset + 1];
         int number = 0;
         int idx1, idx2;
+
         if (start.getIndex() < 0) {
+
             for (Route city : Route.values()) {
                 number = getNumber(route, offset, number, city);
             }
+
             if (start == OPTIONAL) {
                 number += numberOfTrips(route, offset + 1);
             }
@@ -61,6 +98,7 @@ public class RouteTask {
 
         } else {
             idx1 = start.getIndex();
+
             if (next.getIndex() < 0) {
                 for (Route city : Route.values()) {
                     if (city.getIndex() >= 0) {
@@ -74,16 +112,16 @@ public class RouteTask {
                     number += numberOfTrips(route, offset + 1);
                 }
                 route[offset + 1] = next;
+
             } else {
                 idx2 = next.getIndex();
                 number = circuit[idx1][idx2] == 0 ? 0 : numberOfTrips(route, offset + 1);
             }
-
         }
         return number;
     }
 
-    private int getNumber(Route[] route, int offset, int number, Route city) {
+    private int getNumber(Route[] route, int offset, int number, @NotNull Route city) {
         if (city.getIndex() >= 0) {
             route[offset] = city;
             number += numberOfTrips(route, offset);
@@ -91,9 +129,24 @@ public class RouteTask {
         return number;
     }
 
-    private int shortDistance(int origin, int dest, boolean[][] flags, int shortest, int distance, boolean sameCity) {
-        if (!sameCity && origin == dest) return distance;
+    /**
+     * a conventional algorithm
+     * <p>
+     * I'll continue to learn.
+     *
+     * @param origin
+     * @param dest
+     * @param flags
+     * @param shortest
+     * @param distance
+     * @param equalCity
+     * @return
+     */
+    private int shortDistance(int origin, int dest, boolean[][] flags, int shortest, int distance, boolean equalCity) {
+
+        if (!equalCity && origin == dest) return distance;
         int currentDistance;
+
         for (int next = 0; next < circuit.length; next++) {
             if (!flags[origin][next] && circuit[origin][next] != 0) {
                 flags[origin][next] = true;
@@ -107,12 +160,23 @@ public class RouteTask {
         return shortest;
     }
 
-    private int numberOfRoutes(int origin, int dest, final int maxDistance, int distance, boolean sameCity) {
+    /**
+     * @param origin
+     * @param dest
+     * @param maxDistance
+     * @param distance
+     * @param equalCity
+     * @return
+     */
+    private int numberOfRoutes(int origin, int dest, final int maxDistance, int distance, boolean equalCity) {
+
         if (distance >= maxDistance) return 0;
         int number = 0;
-        if (!sameCity && origin == dest && distance < maxDistance) {
+
+        if (!equalCity && origin == dest && distance < maxDistance) {
             number++;
         }
+
         for (int next = 0; next < circuit.length; next++) {
             if (circuit[origin][next] != 0) {
                 number += numberOfRoutes(next, dest, maxDistance, distance + circuit[origin][next], false);
